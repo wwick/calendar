95de995c5010d1548eeba170acd3590b732b5759
Elijah Pena <elijahpena@ip-172-31-45-140.us-east-2.compute.internal>
2019-03-06 11:03:52 -0600

    login.php edited for json

--- login.php
+++ login.php
@@ -1,9 +1,14 @@
 <?php
 //Checks to make sure you are indeed a registered user
 require 'database.php';
+header("Content-Type: application/json");
 session_start();
-$user = $_POST["user"];
-$password = $_POST["password"];
+
+$json_str = file_get_contents('php://input');
+$json_obj = json_decode($json_str, true);
+
+$user = $json_obj["user"];
+$password = $json_obj["password"];
 //hashes your password to be compared against the hash stored in the table later
 $hashedPass = password_hash($password, PASSWORD_DEFAULT);
 
@@ -17,10 +22,20 @@ while($stmt->fetch()){
 	if($user === $tableUser){//checks if password hashes match
 		if(password_verify($password, $tablePass)){
 			$_SESSION["user"] = $user_id;
+
+			echo json_encode(array(
+				"success" => true
+			));
+			exit;
+		} else {
+			echo json_encode(array(
+				"success" => false,
+				"message" => "Incorrect Username or Password"
+			));
+			exit;
 		}
 	}
 }
 $stmt->close();
-header("Location:homepage.php");//redirects to homepage after login
 
 ?>
