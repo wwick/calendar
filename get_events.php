c0b9f9aaaa69e00c669dc5fb7f0560b31d75de23
wwick <wwick@wustl.edu>
2019-03-06 19:45:55 -0600

    getting rid of session variables

--- get_events.php
+++ get_events.php
@@ -1,17 +1,15 @@
 <?php
-session_start();
-if (isset($_SESSION['user'])) {
-	require 'database.php';
-	$user_id=$_SESSION['user'];
-	$result = $mysqli->query("SELECT * FROM events WHERE user_id=\"{$user_id}\"");
-	$rows = array();
+
+require 'database.php';
+$user_id = file_get_contents('php://input');
+$result = $mysqli->query("SELECT * FROM events WHERE user_id=\"{$user_id}\"");
+$rows = array();
 $i=0; 
 while($row = $result->fetch_assoc()) {
 	$event_id = $row['event_id'];
 	$rows[$event_id]['title'] = $row['title'];
 	$rows[$event_id]['date'] = $row['date'];
 	$rows[$event_id]['time'] = $row['time']; 
-	
 }
 
 echo json_encode($rows);
