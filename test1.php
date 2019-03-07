58833c09fc9731b1d463459a7ef0544fab6c96ca
wwick <wwick@wustl.edu>
2019-03-06 19:19:25 -0600

    testing get_events

---
+++ test1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Test</title>
+	<!-- <link rel="stylesheet" type="text/css" href="stylesheet.css"> -->
+	<!-- <script src="login.js"></script> -->
+</head>
+<body>
+
+<?php
+
+session_start();
+$_SESSION['user'] = 1;
+
+
+?>
+
+<script src="test.js"></script>
+
+</body>
+</html>
