f627d58650a3c2ac9eaa6584c86848292f8f18d1
wwick <wwick@wustl.edu>
2019-03-18 18:03:28 -0500

    got rid of order by in get_events

--- get_events.php
+++ get_events.php
@@ -10,7 +10,7 @@ if(!isset($_SESSION['user'])) {
 $user_id = $_SESSION['user'];
 $date = file_get_contents('php://input');
 
-$result = $mysqli->query("SELECT title, time FROM events WHERE user_id=\"{$user_id}\" AND date=\"{$date}\" ORDER BY time");
+$result = $mysqli->query("SELECT title, time FROM events WHERE user_id=\"{$user_id}\" AND date=\"{$date}\"");
 $rows = array();
 
 while($row = $result->fetch_assoc()) {
