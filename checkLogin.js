f089b8ce83db161eadaae36cd4184326993ff8a5
wwick <wwick@wustl.edu>
2019-03-18 17:51:39 -0500

    reverting

--- checkLogin.js
+++ checkLogin.js
@@ -7,7 +7,9 @@ function checkLogin() {
 		.then(data => {
 			console.log(data.loggedIn ? "User Logged in" : "No user logged in");
 			console.log("checked login");
-			createCalendar(data.user_id, new Data(2019, 2);
+			createCalendar(data.user_id, new Data(2019, 2));
+		}).catch( function(error) {
+			console.log(error);
 		});
 }
 document.addEventListener("DOMContentLoaded", function(){
