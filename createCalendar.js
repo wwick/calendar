01996a3344324bb8b3ffa58f59540a7884dd399e
wwick <wwick@wustl.edu>
2019-03-18 23:08:43 -0500

    trying to make first week

--- createCalendar.js
+++ createCalendar.js
@@ -10,20 +10,62 @@ function createCalendar(date) {
     $(document.body).append($calendar);
     let month = date.getMonth();
     let year = date.getFullYear();
-    console.log(month);
-    console.log(year);
-	for (let day = 1; day <= days_in_month; day++) {
-		date = new Date(year,month,day);
-		console.log(date.toString());
-		let date_string = date.toISOString().substring(0,10);
-		let $day_div = $("<div>", {id:date_string, 'class':'day', text:date_string});
-		$calendar.append($day_div);
-		fetchEvents(date_string);
+   
+	$table = $("<table>", {'class':'calendar_table'});
+	$head = $("<thead>");
+	$head_row =$("<tr>");
+	$head.append($head_row);
+	for (let day = 0; day < 7; day++) {
+		$head_label = $("<th>", {text:getDayName(day)});
+		$head_row.append($head_label);
 	}
+
+
+	$table.append(getWeek(date));
+
+	$calendar.append($table);
+
+
+
+	// for (let day = 1; day <= days_in_month; day++) {
+	// 	date = new Date(year,month,day);
+	// 	console.log(date.toString());
+	// 	let date_string = date.toISOString().substring(0,10);
+	// 	let $day_div = $("<div>", {id:date_string, 'class':'day', text:date_string});
+	// 	$calendar.append($day_div);
+	// 	fetchEvents(date_string);
+	// }
 }
 
-function getDayName(date) {
-	switch (date.getDay()) {
+
+function getWeek(first_date) {
+	$row = $("<tr>");
+	let month = first_date.getMonth();
+	let year = first_date.getFullYear();
+	let day = first_date.getDate();
+
+	let date = new Date(year,month,day);
+	for (let i = 0; i < date.getDay(); i++) {
+		$row.append($("<td>"));
+	}
+
+	let start = date.getDay();
+
+	for (let i = start; i < 7; i++) {
+		let date = new Date(year,month,day);
+		let date_string = date.toISOString().substring(0,10);
+		let $day_box = $("<td>", {id:date_string, 'class':'day', text:date_string});
+		$row.append($day_box);
+		day += 1;
+	}
+
+	return $row;
+
+}
+
+
+function getDayName(day) {
+	switch (day) {
 		case 0:
 			return "Sunday";
 		case 1:
