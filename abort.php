98d8fbe862f0187dcb73120ecf64b9421d3854bb
wwick <wwick@wustl.edu>
2019-03-20 03:01:28 -0500

    making it look nicer

--- abort.php
+++ abort.php
@@ -2,5 +2,7 @@
     //ensures that session variables are erased
     session_start();
     session_destroy();
-    header("Location:calendar.html");
+    echo json_encode(array(
+        success => true
+    ));
 ?>
--- createHTML.js
+++ createHTML.js
@@ -42,12 +42,44 @@ function createLogin() {
 
 }
 
+function createModifyEventForm(event_id) {
+	
+	$( ".modify_event" ).remove();
+	let $modify_event = $("<div>", {"class":"modify_event"});
+	$modify_event.append($("<br>"));
+	
+	let $header = $("<h3>", {text:"Modify Event Form"});
+	$modify_event.append($header);
+	$modify_event.append("Title: ");
+	let $title_field = $("<input>", {type:"text", id:"new_title"});
+	$modify_event.append($title_field);
+	$modify_event.append("<br>");
+
+	$modify_event.append("Date: ");
+	let $date_field = $("<input>", {type:"date", id:"new_date"});
+	$modify_event.append($date_field);
+	$modify_event.append("<br>");
+
+	$modify_event.append("Time: ");
+	let $time_field = $("<input>", {type:"time", id:"new_time"});
+	$modify_event.append($time_field);
+	$modify_event.append("<br>");
+
+	let $modify_event_btn = $("<button>", {class:"button", type:"submit", id:"modify_event_btn", text:"Modify Event"});
+	$modify_event.append($modify_event_btn);
+	$(document.body).append($modify_event);
+	$modify_event_btn.click(function() {
+		modifyEvent(event_id);
+	});
+}
+
 function createButtons() {
 
+	//create event form
 	$create_event = $("<div>");
 
-	let $header = $("<h3>", {text:"Create Event Form"});
-	$create_event.append($header);
+	let $create_header = $("<h3>", {text:"Create Event Form"});
+	$create_event.append($create_header);
 
 	$create_event.append("Title: ");
 	let $title_field = $("<input>", {type:"text", id:"event"});
@@ -55,12 +87,12 @@ function createButtons() {
 	$create_event.append("<br>");
 
 	$create_event.append("Date: ");
-	let $date_field = $("<input>", {type:"text", id:"date"});
+	let $date_field = $("<input>", {type:"text", id:"date", value:"yyyy-mm-dd"});
 	$create_event.append($date_field);
 	$create_event.append("<br>");
 
 	$create_event.append("Time: ");
-	let $time_field = $("<input>", {type:"text", id:"time"});
+	let $time_field = $("<input>", {type:"text", id:"time", value:"hh-mm-ss"});
 	$create_event.append($time_field);
 	$create_event.append("<br>");
 
@@ -75,13 +107,16 @@ function createButtons() {
 	$create_event.append("<br>");
 
 	let $event_btn = $("<button>", {class:"button", type:"submit", id:"event_btn", text:"Create Event"});
-
 	$create_event.append($event_btn);
-
 	$(document.body).append($create_event);
 
-	$share = $("<div>");
 
+
+	//share calendar form
+	$share = $("<div>");
+	
+	let $share_header = $("<h3>", {text:"Share Calendar"});
+	$share.append($share_header);
 	$share.append("Share Calendar with User: ");
 	let $share_field = $("<input>", {type:"text", id:"cal_user"});
 	$share.append($share_field);
@@ -92,7 +127,11 @@ function createButtons() {
 	$share.append($share_btn);
 	$(document.body).append($share);
 
+
+	//join group form
 	$join_group = $("<div>");
+
+	let $join_group_header = $("<h3>", {text:"Join Group"});
 	$join_group.append("Group ID: ");
 	let $group_field = $("<input>", {type:"number", id:"join_group", min:"0", step:"1"});
 	$join_group.append($group_field);
@@ -103,11 +142,27 @@ function createButtons() {
 	$join_group.append("<br>");
 	$(document.body).append($join_group);
 
+
+	//logout button
 	let $logout = $("<div>");
-	let $logout_btn = $("<a>", {class:"button", text:"logout", href:"abort.php"});
+	$logout.append("<br>");
+	let $logout_btn = $("<button>", {class:"button", text:"Logout"});
 	$logout.append($logout_btn);
 	$(document.body).append($logout);
 
+
+	// gets rid of session variables and returns to login screen
+	$logout_btn.click(function() {
+		fetch(abort.php)
+			.then(response => response.json())
+			.catch(function(error) {
+				console.log(error);
+			});
+		$(document.body).remove();
+		checkLogin();
+	});
+
+	// adds other event listeners
 	document.getElementById("event_btn").addEventListener("click", createEvent, false);
 	document.getElementById("share_btn").addEventListener("click", shareCal, false);
 	document.getElementById("join_btn").addEventListener("click", joinGroup, false);
