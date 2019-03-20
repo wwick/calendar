ec52d640f11a9bb5bb10b9cbebbd3fc086931738
wwick <wwick@wustl.edu>
2019-03-20 01:17:11 -0500

    deleted deprecated files, added stylesheet

--- database.sql
+++ database.sql
@@ -2,17 +2,25 @@ CREATE TABLE `users` (
   `user_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
   `user` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
-  PRIMARY KEY (`user_id`),
-  UNIQUE KEY `user` (`user`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `group_id` smallint(5) unsigned DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 CREATE TABLE `events` (
   `event_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` smallint(5) unsigned NOT NULL,
   `title` varchar(50) NOT NULL,
-  `time` time NOT NULL,
   `date` date NOT NULL,
+  `time` time NOT NULL,
+  `shared_users` text,
+  `group_id` smallint(5) unsigned DEFAULT NULL,
   PRIMARY KEY (`event_id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `events_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+
+CREATE TABLE `viewable` (
+  `shared_user_id` smallint(5) unsigned NOT NULL,
+  `owner_user_id` smallint(5) unsigned NOT NULL,
+  PRIMARY KEY (`shared_user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
--- show.js
+++
@@ -1,8 +0,0 @@
-function show(){
-	console.log("show");
-	var show = document.getElementsByClassName("show");
-	var i = 0;
-	for (i = 0; i < show.length; i++){
-		show[i].style.display = "block";
-	}
-}
