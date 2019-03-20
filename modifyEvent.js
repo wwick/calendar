f44ffb32506c57df2555a73320ab4d0e903744f0
Elijah Pena <elijahpena@ip-172-31-45-140.us-east-2.compute.internal>
2019-03-20 03:41:03 -0500

    Deletion works

--- modifyEvent.js
+++ modifyEvent.js
@@ -1,25 +1,26 @@
-function modifyEvent(event_id) {
-	
+function modifyEvent(event_id, operator) {
+
 	console.log("modifying event " + event_id);
 	const title = $( "#new_title" ).val();
 	const date = $( "#new_date" ).val();
 	const time = $( "#new_time" ).val() + ":00";
 	const token = document.getElementById("token").value;
 
-	const data = { 'title': title, 'time': time, 'date': date, 'event_id':event_id, 'token':token };
+	const data = { 'title': title, 'time': time, 'date': date, 'event_id':event_id, 'token':token, 'operator':operator };
 	fetch("modifyEvent.php", {
 		method: 'POST',
 		body: JSON.stringify(data),
 		headers: { 'content-type': 'application/json' }
 	})
-	.then(response => response.json())
-	.then(data => {
-		console.log(data.success ? "Event added" : "Event not added");
-		let year = date.substring(0,4);
-		let month = date.substring(6,7);
-		console.log(month);
-		console.log(year);
-		createCalendar(new Date(year,month-1));
-	}).catch(error => console.log(error));
+		.then(response => response.json())
+		.then(data => {
+			console.log(data.success ? "Event added" : "Event not added");
+			let year = date.substring(0,4);
+			let month = date.substring(6,7);
+			console.log(month);
+			console.log(year);
+			createCalendar(new Date(year,month-1));
+
+		}).catch(error => console.log(error));
 
 }
