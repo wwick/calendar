c56d307ee8d421e45c1db666ab256e125763edd2
wwick <wwick@wustl.edu>
2019-08-22 21:26:03 -0700

    shortened return blocks

--- validateInput.js
+++ validateInput.js
@@ -3,31 +3,19 @@ function validDate(date) {
 	let regex = "^\\d{4}\\-(0[1-9]|1[012])\\-(0[1-9]|[12][0-9]|3[01])$";
 	regex = new RegExp(regex);
 	let match = date.match(regex);
-	if (match != null) {
-		return true;
-	} else {
-		return false;
-	}
+	return (match != null);
 }
 
 function validTime(time) {
 	let regex = "^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$";
 	regex = new RegExp(regex);
 	let match = time.match(regex);
-	if (match != null) {
-		return true;
-	} else {
-		return false;
-	}
+	return (match != null);
 }
 
 function validTitle(title) {
 	let regex = "/[;<>$]/g";
 	regex = new RegExp(regex);
 	let match = title.match(regex);
-	if (match == null) {
-		return true;
-	} else {
-		return false;
-	}
+	return (match == null);
 }
