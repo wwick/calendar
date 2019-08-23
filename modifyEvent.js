2a3977b76a5620e7a14b375edc2e24941f70a80e
wwick <wwick@wustl.edu>
2019-08-22 21:46:26 -0700

    deleting works

--- modifyEvent.js
+++ modifyEvent.js
@@ -1,21 +1,21 @@
 function modifyEvent(event_id, operator) {
 
-	console.log("modifying event " + event_id);
-	const title = $( "#new_title" ).val();
-	const date = $( "#new_date" ).val();
-	const time = $( "#new_time" ).val();
-	const token = document.getElementById("token").getAttribute("content");
-	console.log(token)
+	// console.log("modifying event " + event_id);
+	// const title = $( "#new_title" ).val();
+	// const date = $( "#new_date" ).val();
+	// const time = $( "#new_time" ).val();
+	// const token = document.getElementById("token").getAttribute("content");
+	// console.log(token)
 
-	if (!validDate(date)) {
-		return;
-	} 
-	if (!validTime(time)) {
-		return;
-	}
-	if (!validTitle(title)) {
-		return;
-	}
+	// if (!validDate(date)) {
+	// 	return;
+	// } 
+	// if (!validTime(time)) {
+	// 	return;
+	// }
+	// if (!validTitle(title)) {
+	// 	return;
+	// }
 
 	const data = { 'title': title, 'time': time, 'date': date, 'event_id':event_id, 'token':token, 'operator':operator };
 	fetch("modifyEvent.php", {
